7c2c31ec2b53322d8f81305c93dc4c0dbb27a146
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-06-08 11:22:21 -0400

    Move helper function into util.js for consistency

--- cypress/support/__template/env/config.js
+++ cypress/support/__template/env/config.js
@@ -1,8 +1,8 @@
-import { _resolveActiveEnv } from "./_resolveActiveEnv";
+import { resolveActiveEnv as resolve } from "./util";
 
 // resolve keys for the active environment specified in cypress.env.json
-const APP = _resolveActiveEnv("app");
-const CREDENTIALS = _resolveActiveEnv("credentials");
+const APP = resolve("app");
+const CREDENTIALS = resolve("credentials");
 
 // construct the URL before passing it to consumers
 const endpoint = new URL(APP.url);
@@ -14,7 +14,7 @@ endpoint.port = APP.port;
  * The base at which the app is served
  * e.g. http://localhost:80 or https://example.com:443
  */
-export const APP_URL = endpoint.toString();
+export const APP_URL = endpoint.href;
 
 /**
  * Login credentials
--- cypress/support/__template/env/util.js
+++ cypress/support/__template/env/util.js
@@ -3,11 +3,11 @@
  * @param {string} name
  * @returns
  */
-export const _resolveActiveEnv = (name) => {
+export const resolveActiveEnv = (key) => {
   const activeEnv = Cypress.env("__active_env");
 
   try {
-    return Cypress.env(activeEnv)[name];
+    return Cypress.env(activeEnv)[key];
   } catch (e) {
     throw new Error(
       `Cannot set active environment because '${activeEnv}' does not match any environment in cypress.env.json`
