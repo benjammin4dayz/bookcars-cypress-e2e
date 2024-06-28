73e5feffbefe04d500a0dc8e9658b8a041534fa4
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-06-28 04:12:33 -0400

    Update test spec

--- cypress/e2e/__template.cy.js
+++ cypress/e2e/__template.cy.js
@@ -7,22 +7,39 @@ describe('Template', () => {
 
   it('App URL is valid', () => {
     expect(Home.url).to.be.a('string').and.not.empty;
+    expect(() => new URL(Home.url)).to.not.throw();
   });
 
   it('App URL returns a 200 status', () => {
     cy.request(Home.url).then(response => {
       expect(response.status).to.eq(200);
-      Home.visit();
     });
   });
 
-  it('Username exists', () => {
-    expect(env.USERNAME).to.be.a('string');
-    expect(env.USERNAME).to.not.be.empty;
+  it('Credentials exist', () => {
+    expect(env.USERNAME).to.be.a('string').and.not.empty;
+    expect(env.PASSWORD).to.be.a('string').and.not.empty;
   });
 
-  it('Password exists', () => {
-    expect(env.PASSWORD).to.be.a('string');
-    expect(env.PASSWORD).to.not.be.empty;
+  it('Expected elements exist', () => {
+    Home.visit();
+
+    const { header, description, moreInformation } = Home.elements;
+
+    header.should('exist').and('be.visible').and('have.text', 'Example Domain');
+
+    description
+      .should('exist')
+      .and('be.visible')
+      .and(
+        'have.text',
+        'This domain is for use in illustrative examples in documents. You may use this\n    domain in literature without prior coordination or asking for permission.'
+      );
+
+    moreInformation
+      .should('exist')
+      .and('be.visible')
+      .and('have.text', 'More information...')
+      .and('have.attr', 'href', 'https://www.iana.org/domains/example');
   });
 });
